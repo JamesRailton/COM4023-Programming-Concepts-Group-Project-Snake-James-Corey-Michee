d6dd69cb2fad63184f5d569f3f7768ef988de92a
Michee Kibenge <micheekibenge@gmail.com>
2025-03-21 11:00:43 +0000

    Player movement & tail

---
+++ COM4023-Programming-Concepts-Group-Project-Snake-James-Corey-Miche/Tail.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * @author (Corey Wright, James Railton, Michee Kibenge) 
+ * @version (0.2)
+ */
+public class Tail extends Actor
+{
+    /**
+     * Act - do whatever the Tail wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    int red, green, blue;
+    public Tail(int red, int green, int blue)
+    {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+        getImage().setColor(new Color(red,green,blue));
+        getImage().fillRect(0,0,40,40);
+    }
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
